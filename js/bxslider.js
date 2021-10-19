58365a74d75658c1090d9f0deef20daf9c0a2761
eunnie <90018393+eunnie@users.noreply.github.com>
2021-10-19 13:38:01 +0900

    Add files via upload

---
+++ js/bxslider.js
@@ -0,0 +1,16 @@
+//bxslider script code
+$(function(){
+    $('.bxslider').bxSlider({
+        mode:'horizontal',
+        moveSlides:1,
+        slideMargin:40,
+        infiniteLoop:true,
+        maxSlides:8,
+        speed:400,
+        auto:true,
+        controls:true,
+        responsive:true  
+        //autoDirection:'prev'
+    });
+});
+
